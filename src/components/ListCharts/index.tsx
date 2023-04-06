fd73e45389fafc23f7be1b259b95bb73abff70a9
Peregrinus <zakharov.mark22@gmail.com>
2023-04-06 21:10:50 +0300

    center grid

--- src/components/ListCharts/index.tsx
+++ src/components/ListCharts/index.tsx
@@ -1,5 +1,23 @@
-import { Box } from '@mui/material';
+import { useContext, useEffect, useState } from 'react';
+import Grid from '@mui/material/Grid';
+
+import { Context } from '../../App';
 
 export const ListCharts = () => {
-  return <h1>Settings</h1>;
+  const value = useContext(Context);
+
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: '100vh' }}
+    >
+      <Grid item xs={3}>
+        <h1>{value.chartState[0].title.text}</h1>
+      </Grid>
+    </Grid>
+  );
 };
--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,10 +1,6 @@
 import { ListCharts } from '../../components/ListCharts';
 import { useContext, useEffect, useState } from 'react';
 
-import { Context } from '../../App';
-
 export const Setting = () => {
-  const value = useContext(Context);
-
   return <ListCharts />;
 };
